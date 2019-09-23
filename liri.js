1b07b8022a75664796ee2039d8a2c154b1ea4e23
bcwill <Brigit@brigits-mbp.home>
2019-09-23 16:21:04 -0400

    movies

--- liri.js
+++ liri.js
@@ -38,3 +38,18 @@ function showSong() {
         "\nAlbum the Song is From: " + data.tracks.items[0].album.name);
     });
 };
+
+function showMovie() {
+    request("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log("Title of the Movie: " + JSON.parse(body).Title);
+            console.log("Year: " + JSON.parse(body).Year);
+            console.log("IMBD Rating: " + JSON.parse(body).imdbRating0;
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1]);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+        };
+    });
+};
